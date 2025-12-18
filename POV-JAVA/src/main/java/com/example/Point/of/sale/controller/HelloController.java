90300fb31a08d691271b65b9ab7ce006fd7e0ed9
RmiliYahya1 <rmiliyahya2@gmail.com>
2025-12-18 16:44:34 +0100

    commit

--- POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello(){
-        return "Hello from CI/CD :)";
+        return "Hello from CI/CD :(";
     }
 
     @GetMapping("/user")
